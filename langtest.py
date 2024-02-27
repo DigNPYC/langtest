a3cdd7087e055446be76d249cf966c00e8116fd3
麻辣大西瓜 <759824046@qq.com>
2024-02-27 23:10:58 +0800

    测试 ggshield

--- langtest.py
+++ langtest.py
@@ -1,5 +1,39 @@
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(openai_api_key="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")
+from langchain_community.document_loaders import WebBaseLoader
 
-llm.invoke("how can langsmith help with testing?")
+loader = WebBaseLoader("https://docs.smith.langchain.com")
+
+docs = loader.load()
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(openai_api_key="openai_api_key=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")
+
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+from langchain_core.prompts import ChatPromptTemplate
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are world class technical documentation writer."),
+    ("user", "{input}")
+])
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+
